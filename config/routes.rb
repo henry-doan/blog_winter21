57fffe8057f542ba84e9ec38eec8ce21276c673e
Henry Doan <henrydoan96@gmail.com>
2021-11-22 20:55:34 -0700

    end of overview

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  get 'pages/edit'
+  root 'pages#index'
+
+  resources :pages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
